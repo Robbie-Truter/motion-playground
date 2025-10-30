d8067b7632c6809af6dad7137d4d740aa91ea20b
Robbie-Truter <timothylang6@gmail.com>
2025-10-30 23:05:37 +0200

    Setup Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { motion } from "motion/react";
 import "./App.css";
 
-function App() {
+const box = {
+  width: 100,
+  height: 100,
+  backgroundColor: "red",
+  borderRadius: 5,
+};
+
+const App = () => {
   return (
     <>
-      <main className="main-container">
-        <h1>Framer Motion</h1>
+      <main>
+        <section className="space-y-5 bg-[#9bb8cd]">
+          <h1>Hover animations</h1>
+          <div className="flex gap-5">
+            <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={box} />
+            <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} style={box} />
+          </div>
+        </section>
+        <section className="space-y-5 bg-[#fff7d4] text-black">
+          <h1>Button animations</h1>
+          <div>
+            <motion.button
+              className="px-6 py-3 bg-indigo-600 text-white font-semibold rounded-lg shadow-lg"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Click Me!
+            </motion.button>
+          </div>
+        </section>
       </main>
     </>
   );
-}
+};
 
 export default App;
